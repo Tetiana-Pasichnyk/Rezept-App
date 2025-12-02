b2d4ed9ee99240da7d8288ba30682c54de80396e
pasichnykTetiana-create <pasichnyktetiana.de@gmail.com>
2025-12-02 12:02:36 +0100

    New project

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MealPage from "./pages/MealPages/MealPage";
-
+import Nav from "./components/Nav/Nav";
+// <-- импортируем Navbar
 import "@fontsource/happy-monkey";
 
 function App() {
-  return <div className="App">Test Mengjun</div>;
+    return (
+        <BrowserRouter>
+            <Nav /> {/* <-- добавляем Navbar здесь, вне Routes */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/meal/:id" element={<MealPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
-git commit -m "Merge branch 'tetiana' into main"
-
